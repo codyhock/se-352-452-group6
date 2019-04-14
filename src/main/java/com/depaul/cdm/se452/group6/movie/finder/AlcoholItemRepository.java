7b83beee345ab8934b2adad3531679cc22d003cf
CTaylor63 <cptaylor2014@gmail.com>
2019-04-14 17:52:44 -0500

    Added tables for UserType and Alcohol. Added java for these tables and interfaces for searching

---
+++ src/main/java/com/depaul/cdm/se452/group6/movie/finder/AlcoholItemRepository.java
@@ -0,0 +1,14 @@
+package com.depaul.cdm.se452.group6.movie.finder;
+
+import com.depaul.cdm.se452.group6.movie.entity.AlcoholItem;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AlcoholItemRepository extends CrudRepository<AlcoholItem, Long> {
+
+    List<AlcoholItem> findByType(String type);
+
+    List<AlcoholItem> findByPrice(Double price);
+
+}
