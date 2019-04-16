830e9e51fbfef95ad33cc468fa16bf594f3b2577
CHock <chock@growthplay.com>
2019-04-16 18:18:46 -0500

    cody - adding the seats table and java files

---
+++ src/main/java/com/depaul/cdm/se452/group6/movie/finder/SeatRepository.java
@@ -0,0 +1,12 @@
+package com.depaul.cdm.se452.group6.movie.finder;
+
+import com.depaul.cdm.se452.group6.movie.entity.Seat;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SeatRepository extends CrudRepository<Seat, Long> {
+
+
+  List<Seat> findByTheater(int theaterId);
+}
