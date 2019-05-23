4d50f2c9b7d9e23a7d94f170d5742dd552e62840
CHock <chock@growthplay.com>
2019-05-23 13:33:51 -0500

    cody - adding tests for seats and seat types

---
+++ src/test/java/com/depaul/cdm/se452/group6/movie/SeatRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.depaul.cdm.se452.group6.movie;
+
+import com.depaul.cdm.se452.group6.movie.entity.Seat;
+import com.depaul.cdm.se452.group6.movie.entity.SeatType;
+import com.depaul.cdm.se452.group6.movie.entity.Theater;
+import com.depaul.cdm.se452.group6.movie.finder.SeatRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class SeatRepositoryTest {
+
+  @Autowired
+  private TestEntityManager entityManager;
+
+  @Autowired
+  private SeatRepository repository;
+
+  @Test
+  public void testFindAll() {
+    int numOfRows = repository.findAll().size();
+    final int EXPECTED_NUM_OF_ROWS = 80;
+    Assert.assertEquals(numOfRows, EXPECTED_NUM_OF_ROWS);
+  }
+
+  @Test
+  public void testFindByTheater() {
+    Long theater = 1L;
+    int numOfRows = repository.findByTheaterId(theater).size();
+    final int EXPECTED_NUM_OF_ROWS = 20;
+    Assert.assertEquals(numOfRows, EXPECTED_NUM_OF_ROWS);
+  }
+
+  @Test
+  public void testFindById() {
+    Long seatId = 1L;
+    Seat seat = repository.findById(seatId).get();
+
+    long seatNum = seat.getSeatNumber();
+    String avail = seat.getAvailability();
+    SeatType type = seat.getSeatType();
+    Theater theater = seat.getTheater();
+
+    Assert.assertEquals(1, seatNum);
+    Assert.assertEquals("Available", avail);
+    Assert.assertEquals("Handicap", type.getType());
+    Assert.assertEquals(1, theater.getTheater());
+  }
+
+  @Test
+  public void testUpdate() {
+    Long seatId = 1L;
+    Seat found = repository.findById(seatId).get();
+
+    Assert.assertEquals("Available", found.getAvailability());
+
+    found.setAvailability("Unavailable");
+    entityManager.persist(found);
+    entityManager.flush();
+
+    Seat updated = repository.findById(seatId).get();
+
+    Assert.assertEquals("Unavailable", updated.getAvailability());
+  }
+
+
+}
