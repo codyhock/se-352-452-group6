b27138331ea7986fdd8bcc1920d8899b3a81086e
CHock <chock@growthplay.com>
2019-06-06 12:41:03 -0500

    cody - first go at connecting movie selection to seats

--- src/main/java/com/depaul/cdm/se452/group6/movie/service/TheaterService.java
+++ src/main/java/com/depaul/cdm/se452/group6/movie/service/TheaterService.java
@@ -10,16 +10,31 @@ import java.util.List;
 public class TheaterService {
 
   private TheaterRepository theaterRepository;
+  private LogService logService;
 
-  public TheaterService (TheaterRepository theaterRepository) {
+  public TheaterService (TheaterRepository theaterRepository, LogService logService) {
     this.theaterRepository = theaterRepository;
+    this.logService = logService;
+  }
+
+  public Theater getById(Long id) {
+    try {
+      Theater theater = theaterRepository.findByTheater(id.intValue()).get(0);
+      logService.logSuccess("testUser" , "got theater by id " + id);
+      return theater;
+    } catch (Exception e) {
+      logService.logError("testUser", "error getting theater " + id);
+      return null;
+    }
   }
 
   public List<Theater> getTheaters() {
     try {
       List<Theater> theaters = theaterRepository.findAll();
+      logService.logSuccess("testUser" , "got theaters");
       return theaters;
     } catch (Exception e) {
+      logService.logError("testUser", "error getting theaters ");
       return null;
     }
   }
@@ -27,8 +42,10 @@ public class TheaterService {
   public List<Theater> getTheatersByDate(LocalDate date) {
     try {
       List<Theater> theaters = theaterRepository.findByDate(date);
+      logService.logSuccess("testUser" , "got theater by date");
       return theaters;
     } catch (Exception e) {
+      logService.logError("testUser", "error getting theater by date ");
       return null;
     }
   }
